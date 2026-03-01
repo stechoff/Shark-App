2ff671b477bb6d8716a911c465162e055b751cfe
stechoff <horst_steckert@web.de>
2026-03-01 13:02:04 +0100

    Update AylaApiClient.java

--- app/src/main/java/com/sharkcontrol/api/AylaApiClient.java
+++ app/src/main/java/com/sharkcontrol/api/AylaApiClient.java
@@ -22,7 +22,7 @@ import java.util.concurrent.Executors;
 public class AylaApiClient {
 
     private static final String AUTH_BASE    = "https://logineu.sharkninja.com";
-    private static final String API_BASE     = "https://api-eu.sharkninja.com";
+    private static final String API_BASE     = "https://ads-field-eu.aylanetworks.com";
     private static final String CLIENT_ID    = "rKDx9O18dBrY3eoJMTkRiBZHDvd9Mx1I";
     private static final String REDIRECT_URI = "com.sharkninja.shark://com.sharkninja.shark/callback";
     private static final String SCOPE        = "openid profile email offline_access read:users read:current_user read:user_idp_tokens";
